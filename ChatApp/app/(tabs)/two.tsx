d92da05fcfb31992c7cb70f6e1a2ecd34422780d
Jordan Dawson <jtdawson@icloud.com>
2023-09-03 21:32:43 +0000

    Setup basic login, logout, and register with test data for Firebase.

--- ChatApp/app/(tabs)/two.tsx
+++ ChatApp/app/(tabs)/two.tsx
@@ -1,14 +1,64 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Button } from 'react-native';
 import { Text, View } from '../../components/Themed';
+import { getAuth, User,  createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, Auth, UserCredential } from 'firebase/auth';
+
+import app from '../firebaseConfig';
 
 export default function TabTwoScreen() {
+  const [user, setUser] = useState<User | null>(null);
+  const auth: Auth = getAuth(app);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  const handleSignUp = async () => {
+    try {
+      const userCredential: UserCredential = await createUserWithEmailAndPassword(auth, 'test@example.com', 'password');
+      const newUser = userCredential.user;
+      setUser(newUser);
+    } catch (error) {
+      console.error('Error signing up:', error.message);
+    }
+  };
+
+  const handleSignIn = async () => {
+    try {
+      const userCredential: UserCredential = await signInWithEmailAndPassword(auth, 'test@example.com', 'password');
+      const signedInUser = userCredential.user;
+      setUser(signedInUser);
+    } catch (error) {
+      console.error('Error signing in:', error.message);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error('Error signing out:', error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      {user ? (
+        <Button onPress={handleSignOut} title='Log out' />
+      ) : (
+        <>
+          <Button onPress={handleSignUp} title='Register' />
+          <Button onPress={handleSignIn} title='Login' />
+        </>
+      )}
+      <Button onPress={() => console.log('Forgot password?')} title='Forgot password?' />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
 }
